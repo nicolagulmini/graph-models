e1758b57412f9af7f5002e508435e095744e3b81
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-24 17:10:30 +0100

    Update Barabási–Albert model.py

--- Barabási–Albert model.py
+++ Barabási–Albert model.py
@@ -1,6 +1,5 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-import random
+from random import uniform
 
 g = nx.Graph()
 
@@ -9,13 +8,16 @@ g.add_edge(*(0, 1))
 
 def BA_model(g, final_number_of_nodes=20, alpha=1.):
     while g.number_of_nodes() < final_number_of_nodes:
-        probabilities = [g.degree(node)/(2*g.number_of_edges()) for node in g.nodes]
-        coin_flips = [random.uniform(0, 1) for _ in range(len(probabilities))]
+        # allow auto loops?
+        # allow nodes with a clearly opposite behaviour
+        den = sum([g.degree(node)**alpha for node in g.nodes])
+        probabilities = [g.degree(node)**alpha/den for node in g.nodes]
+        coin_flips = [uniform(0, 1) for _ in range(len(probabilities))]
         new_node = g.number_of_nodes()
         g.add_node(new_node)
         for i in range(new_node):
             if coin_flips[i] < probabilities[i]:
                 g.add_edge(*(new_node, i))
-
+        
 BA_model(g)
 nx.draw(g, with_labels=True)
