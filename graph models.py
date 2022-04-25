26579886b10eea9f8412af3470ad19cf4a06c6c0
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-25 04:02:15 +0100

    update

--- graph models.py
+++ graph models.py
@@ -1,15 +1,22 @@
 import networkx as nx
 from pyvis.network import Network
-from random import uniform
+from random import uniform, sample
 import itertools
 
-# Erdős–Rényi model
-def ER_model(number_of_nodes, edge_probability):
+# Erdős–Rényi models
+def ER_model_Gnp(number_of_nodes, edge_probability):
     g = nx.Graph()
     g.add_nodes_from(range(number_of_nodes))
     for el in itertools.combinations(range(number_of_nodes), 2):
         if uniform(0, 1) < edge_probability:
             g.add_edge(*el)
+    return g
+
+def ER_model_GnM(number_of_nodes, number_of_edges): # clearly number_of_edges has to be <= n(n-1)/2
+    g = nx.Graph()
+    g.add_nodes_from(range(number_of_nodes))
+    sampled_edges = sample([pair for pair in itertools.combinations(range(number_of_nodes), 2)], number_of_edges)
+    g.add_edges_from(sampled_edges)
     return g
     
 # Watts–Strogatz model
@@ -35,7 +42,7 @@ def BA_model(g, final_number_of_nodes=30, alpha=1.):
 
 # BA_model(g)
 
-g = ER_model(100, .01)
+g = ER_model_GnM(100, 100)
 
 net = Network('1000px', '2000px')
 net.from_nx(g)
@@ -45,6 +52,7 @@ net.show("my_graph.html")
 # - expected degree of the node i at the j-th iteration
 # - number of isolated nodes
 # - growth of number of edges 
+# - degree distribution
 
 # allow auto loops?
 # allow nodes with a clearly opposite behaviour
